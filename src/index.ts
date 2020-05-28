1c963493a2f119555e302f8d60bd018fad35fcfa
Quentin Wright <wrightq00@gmail.com>
2020-05-28 15:10:16 -0500

    misc: remove sharding hack

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import { ShardingManager, Shard } from 'discord.js';
 import { Logger } from './structures/util/Logger';
 require('dotenv').config({ path: `${__dirname}/../.env` });
 
-let ready = 0;
-
 const options = {
-    totalShards: parseInt(process.env.pshards ?? '2'),
+    totalShards: parseInt(process.env.shards ?? '2'),
     respawn: true,
     mode: 'worker',
     execArgv: ['--expose-gc', '--trace-warnings', '--max_old_space_size=24576', '--experimental-worker', '--inspect']
@@ -14,7 +12,7 @@ const options = {
 const manager = new ShardingManager('./build/start.js', options as any);
 manager.on('shardCreate', (shard: Shard) => {
     shard.on('spawn', () => Logger.info(`Shard ${shard.id} spawned`));
-    shard.on('ready', () => Logger.info(`Shard ${shard.id} ready` && ready++));
+    shard.on('ready', () => Logger.info(`Shard ${shard.id} ready`));
     shard.on('disconnect', () => Logger.error(`Shard ${shard.id} disconnected`));
     shard.on('error', (error: Error) => Logger.error(`Shard ${shard.id} error: ${error}`));
 });
