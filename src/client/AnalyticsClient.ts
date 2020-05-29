05937225711390e6e3cb43e735045e978410fdb9
Quentin Wright <wrightq00@gmail.com>
2020-05-28 20:33:22 -0500

    fix: update intents

--- src/client/AnalyticsClient.ts
+++ src/client/AnalyticsClient.ts
@@ -67,7 +67,7 @@ export class AnalyticsClient extends AkairoClient {
             partials: ['CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION', 'USER'],
             fetchAllMembers: true,
             ws: {
-                intents: new Intents(['GUILD_PRESENCES', 'GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS'])
+                intents: Intents.ALL
             }
         });
     }
