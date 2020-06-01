aa3d6be45d27c69513a0662a9dc2a33456c474a1
Quentin Wright <wrightq00@gmail.com>
2020-06-01 14:51:21 -0500

    refactor: argument types

--- src/commands/stats/channel.ts
+++ src/commands/stats/channel.ts
@@ -1,5 +1,5 @@
 import { Command } from 'discord-akairo';
-import { Message, MessageEmbed } from 'discord.js';
+import { Channel, Message, MessageEmbed } from 'discord.js';
 import { getRepository } from 'typeorm';
 import { Channel as CEntity, Guild as GEntity, Message as MEntity } from '../../structures/db';
 
@@ -15,31 +15,33 @@ export default class ChannelCommand extends Command {
             clientPermissions: ['EMBED_LINKS', 'SEND_MESSAGES'],
             args: [
                 {
-                    id: 'id',
-                    default: (message: Message) => message.channel.id
+                    id: 'channel',
+                    type: 'channel',
+                    default: (message: Message) => message.channel
                 }
             ]
         });
     }
 
-    public async exec(message: Message, { id }: { id: string }): Promise<Message | Message[]> {
-        if (!id) return message.util!.send('Invalid channel ID, canceling.');
+    public async exec(message: Message, { channel }: { channel: Channel }): Promise<Message | Message[]> {
+        const id = channel.id;
 
-        const channel = await getRepository(CEntity).findOne({ id }) || this.client.channels.cache.get(id);
+        const channelRecord: any = await getRepository(CEntity).findOne({ id }) || this.client.channels.cache.get(id);
         const guildRecord = await getRepository(GEntity).findOne({ id: channel instanceof CEntity ? channel.guild : (channel as any).guild.id });
         const messages = await getRepository(MEntity).count({ channel: id });
+
         if (!channel) return message.util!.send('A channel record or client channel with that ID was not found.');
         if (channel instanceof CEntity) {
             const embed = new MessageEmbed()
-                .setTitle(`Stats for ${channel.name || channel.id}`)
-                .setDescription(`Using record #${channel.uid}`)
-                .addField('Name (ID)', `${channel.name} (${channel.id})`)
-                .addField('Type', channel.type)
+                .setTitle(`Stats for ${channelRecord.name || channelRecord.id}`)
+                .setDescription(`Using record #${channelRecord.uid}`)
+                .addField('Name (ID)', `${channelRecord.name} (${channelRecord.id})`)
+                .addField('Type', channelRecord.type)
                 .addField('Message Count', messages)
-                .addField('Parent ID', channel.parent);
+                .addField('Parent ID', channelRecord.parent);
             if (guildRecord) embed.addField('Guild (ID)', `${guildRecord.name} (${guildRecord.id})`);
-            embed.addField('Topic', channel.topic || 'None')
-                .addField('Created At', new Date(channel.createdAt).toLocaleString());
+            embed.addField('Topic', channelRecord.topic || 'None')
+                .addField('Created At', channel.createdAt.toLocaleString());
             return message.util!.send(embed);
         }
 
