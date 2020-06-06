dfa3f44ee0cb731a295dd906e4bd522ab297ab32
Quentin Wright <wrightq00@gmail.com>
2020-06-06 16:24:38 -0500

    feat: move options to .env

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { ShardingManager, Shard } from 'discord.js';
 import { Logger } from './structures/util/Logger';
 require('dotenv').config({ path: `${__dirname}/../.env` });
 
-const options = {
-    totalShards: parseInt(process.env.shards ?? '2'),
+const options: any = {
+    totalShards: parseInt(process.env.shards ?? '0') || 'auto',
     respawn: true,
-    mode: 'process',
-    execArgv: ['--expose-gc', '--trace-warnings', '--max_old_space_size=24576', '--experimental-worker', '--inspect']
+    mode: process.env.shardtype as 'process' | 'worker',
+    execArgv: ['--inspect', '--experimental-worker']
 };
 
-const manager = new ShardingManager('./build/start.js', options as any);
+const manager = new ShardingManager('./build/start.js', options);
 manager.on('shardCreate', (shard: Shard) => {
     shard.on('spawn', () => Logger.info(`Shard ${shard.id} spawned`));
     shard.on('ready', () => Logger.info(`Shard ${shard.id} ready`));
